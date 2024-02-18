3f9eba8dedfe5dd2f2401612195f72841ca48a6c
zakihashin <155668242+zakihashin@users.noreply.github.com>
2024-02-18 19:38:50 +0000

    Add files via upload

---
+++ getsum.py
@@ -0,0 +1,20 @@
+def add(num1,num2):
+    sum1 = num1 + num2
+    return sum1
+def sub(num1,num2):
+    sum2 = num1 - num2
+    return sum2
+def mul(num1,num2):
+    sum3 = num1 * num2
+    return sum3
+def div(num1,num2):
+    sum4 = num1 / num2
+    return sum4
+addition = add(1,54)
+subtraction = sub(546,12)
+multiplication = mul(23,43)
+division = div(44,11)
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
